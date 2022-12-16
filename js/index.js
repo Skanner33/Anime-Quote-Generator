612ec1150a4b66e57f3ae321e0b00c38a0c2c0a0
Skanner33 <mathewmageto@gmail.com>
2022-12-17 00:40:41 +0300

    Login form created

--- js/index.js
+++ js/index.js
@@ -3,18 +3,28 @@
 document.addEventListener('DOMContentLoaded', () => {
    const btn = document.getElementById('btn');
 
-   btn.addEventListener('click', () => {
-    const form = document.getElementById('log-form');
+    btn.addEventListener('click', () => {
+     const form = document.getElementById('log-form');
 
-    if(form.style.display === 'none') {
-        form.style.display = 'block';
-    }else{
-        form.style.display = 'none';
-    }
+     if(form.style.display === 'none') {
+         form.style.display = 'block';
+     }else{
+         form.style.display = 'none';
+     }
 
-   });
+    });
 
-   
+ const loginBtn = document.getElementById("login-btn");
+ const loginForm = document.getElementById("login-form");
+
+ loginBtn.addEventListener('click', () => {
+  if(loginForm.style.display === "none"){
+    loginForm.style.display = "block";
+  } else{
+    loginForm.style.display = "none";
+  }
+});
+
 });
 
 
