317c0ffe51b430b1bd0f4c430d9e9827a124e9e4
Skanner33 <mathewmageto@gmail.com>
2022-12-17 15:57:59 +0300

    API endpoint added

--- js/index.js
+++ js/index.js
@@ -1,32 +1,66 @@
-// const api
+//const api
+const API = "https://animechan.vercel.app/api/random";
 
-document.addEventListener('DOMContentLoaded', () => {
-   const btn = document.getElementById('btn');
+const pagelist = document.getElementById("pagelist");
 
-    btn.addEventListener('click', () => {
-     const form = document.getElementById('log-form');
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btn");
 
-     if(form.style.display === 'none') {
-         form.style.display = 'block';
-     }else{
-         form.style.display = 'none';
-     }
+  btn.addEventListener("click", () => {
+    const form = document.getElementById("log-form");
 
-    });
+    if (form.style.display === "none") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+  });
 
- const loginBtn = document.getElementById("login-btn");
- const loginForm = document.getElementById("login-form");
+  const loginBtn = document.getElementById("login-btn");
+  const loginForm = document.getElementById("login-form");
 
- loginBtn.addEventListener('click', () => {
-  if(loginForm.style.display === "none"){
-    loginForm.style.display = "block";
-  } else{
-    loginForm.style.display = "none";
-  }
-});
+  loginBtn.addEventListener("click", () => {
+    if (loginForm.style.display === "none") {
+      loginForm.style.display = "block";
+    } else {
+      loginForm.style.display = "none";
+    }
+  });
 
-});
+//   function getQuote() {
+//     //http request to API endpoint
+//     fetch(API)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         //extract the quote, character and author
+//         const anime = data.anime;
+//         const character = data.character;
+//         const quote = data.quote;
 
+//         //create element to hold quote, character and author
+//         const quoteElement = document.createElement("p");
+//         quoteElement.innerHTML = `"${quote}" - ${anime} - ${character}`;
 
+//         //bind event listeners to the share, save and like buttons
+//         document.getElementById("quote-of-the-day").appendChild(quoteElement);
+        
+//         document.getElementById("btn-share").addEventListener("click", () => {
+//             //code to share quotes
+//         });
+//         document.getElementById("btn-save").addEventListener("click", () => {
+//             //code to save quotes 
+//         });
+//         document.getElementById("btn-like").addEventListener("click", () => {
+//             //code to like quotes
+//         });
+//       });
+//   }
+//   //calls getQuote function 
+//   getQuote();
 
-  
+//   //bind event listener to generate quote button
+//   document.getElementById('btn-generate').addEventListener('click', () => {
+//     //when button is clicked gets new quote
+//     getQuote();
+//   });
+// });
